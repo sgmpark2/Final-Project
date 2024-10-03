af00af75739761bc0529f6e7f0f4450710ab6bb9
SebastienCB <sgschri3@liverpool.ac.uk>
2024-10-03 14:43:00 +0100

    added weight fraction descriptions

--- Airframe/Weightfractions.m
+++ Airframe/Weightfractions.m
@@ -5,6 +5,7 @@ W_camera=0.269;
 W_gimble=;
 W_empty=;
 W_battery=;
+
 %avionics
 W_pix=0.0158;
 W_motor=;
@@ -17,7 +18,9 @@ W_wiring=;
 W_p=W_camera+W_Gimble;
 W_avionics=W_pix+4*W_motor+4*W_esc+W_receiver+W_Transm+4*W_prop+W_wiring;
 
+%Empty weight fraction
 W_ef=;
+%Battery weight fraction
 W_bf=;
 
 
