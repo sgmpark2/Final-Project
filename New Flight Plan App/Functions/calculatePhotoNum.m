35b471b31aed6bba09e8872fcf22115f51312c35
Chaz Love <sgclove@liverpool.ac.uk>
2025-03-10 12:51:36 +0000

    Added comments.

--- New Flight Plan App/Functions/calculatePhotoNum.m
+++ New Flight Plan App/Functions/calculatePhotoNum.m
@@ -1,5 +1,16 @@
 function [photoNum] = calculatePhotoNum(app)
 
+% This function calculates the number of photos required to reach the
+% inputted % overlap from the user.
+
+% Improvements to be made:
+%   - This all calculates from average distances therefore it is not always
+%   actually giving a 70% overlap 
+%   - Figure out how to actually make it 70% overlap between consecutive
+%   waypoints. This may require combining this function into the
+%   photoProjection function to work dynamically rather than just
+%   calculating from the average.
+
     pathLength = app.stats.pathLength;
     fovH = deg2rad(app.horizontalFOV.Value);
     Disp = app.ProcessedData.Displacement;
