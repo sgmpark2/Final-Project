72849cfa1e4a4fe52e3a70c4eb390ea68e41fedb
sgtjake2 <sgtjake2@liverpool.ac.uk>
2025-03-27 22:17:07 +0000

    Update plots

--- Gimbal_Test_Plot.m
+++ Gimbal_Test_Plot.m
@@ -6,29 +6,44 @@ figure(1)
 subplot(3,1,1)
 hold on
 grid on
-plot(Data.Time/1000,Data.Pitch2/100)
-plot(Data.Time/1000,Data.Pitch/100)
+marker = plot([0 45],[0 0],"Color","Black",LineStyle="-.");
+body = plot(Data.Time/1000,Data.Pitch2/100,"Color","Blue",LineWidth=1.5);
+cam = plot(Data.Time/1000,Data.Pitch/100,"Color","Red",LineWidth=1.5);
 title("Pitch")
 xlabel("Time (Seconds)")
 ylabel("Pitch Angle (Degrees)")
-legend("Airframe Pitch", "Camera Pitch")
+ylim([-22.5 22.5])
+
+h = [body cam];
+l = ["Airframe Pitch" "Camera Pitch"];
+legend(h, l)
 
 subplot(3,1,2)
 hold on
 grid on
-plot(Data.Time/1000,Data.Roll2/100)
-plot(Data.Time/1000,Data.Roll/100)
+marker = plot([0 45],[0 0],"Color","Black",LineStyle="-.");
+body = plot(Data.Time/1000,Data.Roll2/100,"Color","Blue",LineWidth=1.5);
+cam = plot(Data.Time/1000,Data.Roll/100,"Color","Red",LineWidth=1.5);
 title("Roll")
 xlabel("Time (Seconds)")
 ylabel("Roll Angle (Degrees)")
-legend("Airframe Roll", "Camera Roll")
+ylim([-20 20])
+
+h = [body cam];
+l = ["Airframe Pitch" "Camera Pitch"];
+legend(h, l)
 
 subplot(3,1,3)
 hold on
 grid on
-plot(Data.Time/1000,Data.Yaw2/100)
-plot(Data.Time/1000,Data.Yaw/100)
+marker = plot([0 45],[0 0],"Color","Black",LineStyle="-.");
+body = plot(Data.Time/1000,Data.Yaw2/100,"Color","Blue",LineWidth=1.5);
+cam = plot(Data.Time/1000,Data.Yaw/100,"Color","Red",LineWidth=1.5);
 title("Yaw")
 xlabel("Time (Seconds)")
 ylabel("Yaw Angle (Degrees)")
-legend("Airframe Yaw", "Camera Yaw")
+ylim([-40 40])
+
+h = [body cam];
+l = ["Airframe Pitch" "Camera Pitch"];
+legend(h, l)
